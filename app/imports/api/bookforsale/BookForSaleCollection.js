0a336875fc42b9b81fd54c13924f8e07e87a887c
Mark Cummins <MarkCummins@dhcp-168-105-19-126.wireless.manoa.hawaii.edu>
2017-04-28 19:06:29 -1000

    Initial Commit for issue-11

---
+++ app/imports/api/bookforsale/BookForSaleCollection.js
@@ -0,0 +1,5 @@
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+import BaseCollection from '/imports/api/base/BaseCollection';
+import { Book } from '/imports/api/book/BookCollection';
+import { check } from 'meteor/check';
+import { Meteor } from 'meteor/meteor';

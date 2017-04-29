60b4acf27a7465c95d14a71883168cdf9778dc32
Mark Cummins <MarkCummins@dhcp-168-105-19-126.wireless.manoa.hawaii.edu>
2017-04-28 20:57:03 -1000

    Added more functions to the BookForSale Collection

--- app/imports/api/bookforsale/BookForSaleCollection.js
+++ app/imports/api/bookforsale/BookForSaleCollection.js
@@ -3,6 +3,11 @@ import BaseCollection from '/imports/api/base/BaseCollection';
 import { Books } from '/imports/api/book/BookCollection';
 import { check } from 'meteor/check';
 import { Meteor } from 'meteor/meteor';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+import BaseCollection from '/imports/api/base/BaseCollection';
+// 3import { Interests } from '/imports/api/interest/InterestCollection';
+import { check } from 'meteor/check';
+import { Meteor } from 'meteor/meteor';
 
 class BookForSaleCollection extends BaseCollection {
 
@@ -27,7 +32,25 @@ class BookForSaleCollection extends BaseCollection {
 
     Books.assertTitle(title);
     return this._collection.insert({ username, firstName, lastName, title, condition, price, description });
-
+  }
+  findTitle(titleID) {
+    this.assertDefined(titleID);
+    return this.findDoc(titleID).title;
+  }
+  findTitles(titleIDs) {
+    return titleIDs.map(titleID => this.findTitle(titleID));
+  }
+  assertTitle(title) {
+    this.findDoc(title);
+  }
+  assertTitles(titles) {
+    _.each(titles, title => this.assertTitle(title));
+  }
+  findID(title) {
+    return this.findDoc(title)._id;
+  }
+  findIDs(titles) {
+    return (titles) ? titles.map((instance) => this.findID(instance)) : [];
   }
   dumpOne(docID) {
     const doc = this.findDoc(docID);
